4a46ce2fed747e7990cb6bfada5053c3bba13853
Your Name <you@example.com>
2020-03-18 20:07:03 +0000

    script

--- source.sh
+++ source.sh
@@ -2,7 +2,12 @@
 
 
 
+##### PATH
+echo 'export PATH=$PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"' >> /etc/bash.bashrc
 
+
+
+##### SOURCE LIST
 echo "deb http://deb.debian.org/debian buster main contrib non-free
 deb-src http://deb.debian.org/debian buster main contrib non-free
 deb http://security.debian.org/debian-security buster/updates main contrib non-free
@@ -12,6 +17,14 @@ deb-src http://deb.debian.org/debian/ buster-updates main contrib non-free
 deb http://deb.debian.org/debian/ buster-backports main contrib non-free
 deb-src http://deb.debian.org/debian/ buster-backports main contrib non-free" > /etc/apt/sources.list
 
-apt upadte -Y
+
+
+##### UPDATE
+apt update
 apt list --upgradable
-apt upgrade -Y
+apt upgrade -y
+
+
+
+##### UTILS
+apt install net-tools git curl nmap tcpdump -y
