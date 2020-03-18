5901c2daa575f2798436168cd552266ef24bf80c
Your Name <you@example.com>
2020-03-18 19:55:54 +0000

    source list

--- source.sh
+++ source.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-echo "
-deb http://deb.debian.org/debian buster main contrib non-free
+
+
+
+echo "deb http://deb.debian.org/debian buster main contrib non-free
 deb-src http://deb.debian.org/debian buster main contrib non-free
 deb http://security.debian.org/debian-security buster/updates main contrib non-free
 deb-src http://security.debian.org/debian-security buster/updates main contrib non-free
 deb http://deb.debian.org/debian/ buster-updates main contrib non-free
 deb-src http://deb.debian.org/debian/ buster-updates main contrib non-free
 deb http://deb.debian.org/debian/ buster-backports main contrib non-free
-deb-src http://deb.debian.org/debian/ buster-backports main contrib non-free
-" > teste.txt
+deb-src http://deb.debian.org/debian/ buster-backports main contrib non-free" > /etc/apt/sources.list
+
+apt upadte -Y
+apt list --upgradable
+apt upgrade -Y
